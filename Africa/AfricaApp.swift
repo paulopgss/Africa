c65e55ddf497cc37057d3a14cdd326ca9c3833b9
Paulo Sousa <paulopgss@gmail.com>
2023-12-01 21:32:39 -0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Paulo Sousa on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
