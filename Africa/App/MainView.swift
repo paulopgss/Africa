d2f77e494abad91310713eeab7cc8c83fedc5d49
Paulo Sousa <paulopgss@gmail.com>
2023-12-02 13:59:50 -0300

    feat: :art: structure project

--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "square.grid.2x2")
+                    Text("Browse")
+                }
+            
+            VideoListView()
+                .tabItem {
+                    Image(systemName: "play.rectangle")
+                    Text("Watch")
+                }
+            
+            MapView()
+                .tabItem {
+                    Image(systemName: "map")
+                    Text("Locations")
+                }
+            
+            GalleryView()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Gallery")
+                }
+        } //: TAB
     }
 }
 
