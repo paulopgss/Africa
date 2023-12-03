9dfb36d7c6a83c54c9de61f7750e4434517dd131
Paulo Sousa <paulopgss@gmail.com>
2023-12-03 14:54:48 -0300

    feat: :art: map annotation

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,13 +6,77 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
-    var body: some View {
-        Text("Map")
-    }
+  // MARK: - PROPERTIES
+  
+  @State private var region: MKCoordinateRegion = {
+    var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+    var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+    var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+    
+    return mapRegion
+  }()
+  
+  let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+  
+  // MARK: - BODY
+  
+  var body: some View {
+      // TODO: - MapAnnotation is deprecated in iOS 17.0, update to MapContentBuilder
+    Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+      MapAnnotation(coordinate: item.location) {
+        MapAnnotationView(location: item)
+      }
+    }) //: MAP
+    .overlay(
+      HStack(alignment: .center, spacing: 12) {
+        Image("compass")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 48, height: 48, alignment: .center)
+        
+        VStack(alignment: .leading, spacing: 3) {
+          HStack {
+            Text("Latitude:")
+              .font(.footnote)
+              .fontWeight(.bold)
+              .foregroundColor(.accentColor)
+            Spacer()
+            Text("\(region.center.latitude)")
+              .font(.footnote)
+              .foregroundColor(.white)
+          }
+          
+          Divider()
+          
+          HStack {
+            Text("Longitude:")
+              .font(.footnote)
+              .fontWeight(.bold)
+              .foregroundColor(.accentColor)
+            Spacer()
+            Text("\(region.center.longitude)")
+              .font(.footnote)
+              .foregroundColor(.white)
+          }
+        }
+      } //: HSTACK
+      .padding(.vertical, 12)
+      .padding(.horizontal, 16)
+      .background(
+        Color.black
+          .cornerRadius(8)
+          .opacity(0.6)
+      )
+      .padding()
+      , alignment: .top
+    )
+  }
 }
 
+// MARK: - PREVIEW
 #Preview {
     MapView()
 }
---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Paulo Sousa on 03/12/23.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
